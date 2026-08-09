652d52c6d20f649300d1ca131c2b4e507ce2d3a8
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-10 00:30:20 +0700

    Update Header.tsx

--- component/Header.tsx
+++ component/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
                 </div>
             </div>
             <div className="navbar-center">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">TORment</a>
             </div>
             <div className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
