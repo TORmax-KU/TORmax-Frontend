b4cba1889309755bf9b86c451fed2836197f10c6
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-10 00:22:48 +0700

    Update TrackingProjects.tsx
    placeholder

--- component/HomePage/TrackingProjects.tsx
+++ component/HomePage/TrackingProjects.tsx
@@ -2,12 +2,38 @@ export default function TrackingProjects() {
     return (
         <div style={{
             width: '35%',
-            backgroundColor: 'darkblue',
             padding: 15,
             paddingLeft: 25
         }}>
-            
-            TrackingProjects
+
+            Tracked Projects
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Project</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Project A</td>
+                        <td>Approved</td>
+                    </tr>
+                    <tr>
+                        <td>Project B</td>
+                        <td>Applied</td>
+                    </tr>
+                    <tr>
+                        <td>Project C</td>
+                        <td>Rejected</td>
+                    </tr>
+                    <tr>
+                        <td>Project D</td>
+                        <td>Unavailable</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
