5edc6b0f3a6a5ba8e94c44c414e8aa5f5b590d57
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-10 01:06:41 +0700

    Update page.tsx
    don't know tor details yet, only apply design for now

--- app/tor-page/[tor-id]/page.tsx
+++ app/tor-page/[tor-id]/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 
 export default function TORPage() {
   return (
-    <div>
-      TORpage
+    <div style={{
+      display: 'flex',
+      gap: 30,
+      padding: 80,
+      paddingTop: 150
+    }}>
+      <div style={{
+        backgroundColor: 'dimgray',
+        width: '35%',
+        height: 300,
+        padding: 25
+      }}>
+        <>For more
+          information link</>
+        <>pdf link
+        </>
+        <>existing website</>
+        <>Contacts</>
+      </div>
+      <div>
+        Project Info
+      </div>
     </div>
   );
 }
