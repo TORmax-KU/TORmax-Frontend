430aef61f0bee3353790e7a38eb89487bae15663
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-12 14:27:51 +0700

    Update ProjectListingsTabItem.tsx

--- component/HomePage/ProjectRecommendation/ProjectListingsTabItem.tsx
+++ component/HomePage/ProjectRecommendation/ProjectListingsTabItem.tsx
@@ -17,7 +17,7 @@ export default function ProjectListingTabItem({
             <input type="radio" name="my_tabs_2" className="tab" aria-label={label} defaultChecked={defaultChecked} style={{
                 fontSize: 20
             }} />
-            <div className="sticky start-0 tab-content max-w-100 border-base-300 bg-base-100 p-6">{children}</div>
+            <div className="sticky start-0 tab-content max-w-120 border-base-300 bg-base-100 p-6">{children}</div>
         </React.Fragment>
     )
 }
