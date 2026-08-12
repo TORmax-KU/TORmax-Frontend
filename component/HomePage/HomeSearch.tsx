e169267c3add8de7bc29244663717c9725ca830f
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-12 14:34:33 +0700

    Debug HomePage Search unclickable

--- component/HomePage/HomeSearch.tsx
+++ component/HomePage/HomeSearch.tsx
@@ -4,31 +4,36 @@ import SearchInput from "../Searchbar/SearchInput";
 export default function HomeSearch() {
     return (
         <div style={{
-            display: 'flex',
-            position: 'relative',
-            flexDirection: "column",
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: 500
+            height: 500,
+            position: 'relative'
         }}>
             <div style={{
                 position: 'absolute',
                 top: 0,
                 height: '100%',
                 width: '100%',
+                zIndex: 0
             }}>
-            <MediaBlock url="/TORment Header.png" objectFit="cover"/>
+                <MediaBlock url="/TORment Header.png" objectFit="cover" />
             </div>
             <div style={{
-                fontSize: 48,
-                padding: 20,
-                paddingTop: 180,
-                zIndex: 1,
-                textShadow: '2px 4px  3px rgba(0, 0, 0, 0.45)'
+                position: 'relative',
+                display: 'flex',
+                flexDirection: "column",
+                alignItems: 'center',
+                justifyContent: 'center',
+                zIndex: 10,
             }}>
-                What project are you looking for?
+                <div style={{
+                    fontSize: 48,
+                    padding: 20,
+                    paddingTop: 180,
+                    textShadow: '2px 4px  3px rgba(0, 0, 0, 0.45)'
+                }}>
+                    What project are you looking for?
+                </div>
+                <SearchInput />
             </div>
-            <SearchInput />
         </div>
     )
 }
