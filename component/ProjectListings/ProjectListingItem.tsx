50b6ebeff6e880b8bfa19f7923b4f9897cff1419
Karczel <92095164+Karczel@users.noreply.github.com>
2026-08-12 14:45:09 +0700

    Update ProjectListingItem.tsx

--- component/ProjectListings/ProjectListingItem.tsx
+++ component/ProjectListings/ProjectListingItem.tsx
@@ -5,15 +5,14 @@ import { DetailedVersion } from "./ProjectListing";
 export default function ProjectListingItem({ detailed = false }: DetailedVersion) {
     // accept TOR obj
     return (
-        <Link href="/tor-page/1" style={{
+        <li className="list-row" style={{
             width: '100%'
         }}>
-            <li className="list-row" style={{
-                width: '100%'
-            }}>
+            <Link href="/tor-page/1"
+                className="flex items-center gap-2 w-full px-4 py-2 hover:bg-base-300 rounded-lg">
                 <div><RiFileList2Line /></div>
                 <div>
-                    <div className="font-semibold text-base truncate w-[150px]">Digital Skills Training for Rural Youth</div>
+                    <div className="font-semibold text-base truncate w-[200px]">Digital Skills Training for Rural Youth</div>
                     <div className="text-xs uppercase font-semibold opacity-60">Employer</div>
                 </div>
                 {detailed ?
@@ -22,14 +21,15 @@ export default function ProjectListingItem({ detailed = false }: DetailedVersion
                     </div>) : (null)
                 }
 
-                <button className="btn btn-square btn-ghost">
-                    <RiAddCircleFill />
-                </button>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                </button>
+            </Link >
+            <div/>
+            <button className="btn btn-square btn-ghost">
+                <RiAddCircleFill />
+            </button>
+            <button className="btn btn-square btn-ghost">
+                <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+            </button>
 
-            </li>
-        </Link >
+        </li>
     )
 }
